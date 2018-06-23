9ca5a0eba8433094baf512e3102d10bf1fdfc437
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-06-23 17:05:27 -0400

    Replaced most defines with enums

--- include/MCP2515_const.h
+++ include/MCP2515_const.h
@@ -9,203 +9,205 @@
 
 #define DEBUG_MODE 1
 
-#define TIMEOUTVALUE    50
-#define MCP_SIDH        0
-#define MCP_SIDL        1
-#define MCP_EID8        2
-#define MCP_EID0        3
+namespace wlp {
 
-#define MCP_TXB_EXIDE_M     0x08                                        // In TXBnSIDL
-#define MCP_DLC_MASK        0x0F                                        // 4 LSBits
-#define MCP_RTR_MASK        0x40                                        // (1<<6) Bit 6
+namespace Bits {
+    enum {
+        SIDH = 0,  // Standard ID high
+        SIDL = 1,  // Standard ID low
+        EIDH = 2,  // Extended ID high
+        EIDL = 3  // Extended ID low
+    };
+}
 
-#define MCP_RXB_RX_ANY      0x60
-#define MCP_RXB_RX_EXT      0x40
-#define MCP_RXB_RX_STD      0x20
-#define MCP_RXB_RX_STDEXT   0x00
-#define MCP_RXB_RX_MASK     0x60
-#define MCP_RXB_BUKT_MASK   (1<<2)
+namespace Mask {
+    enum {
+        ExtendedID = 0x08,
+        DLC = 0x0F,
+        RemoteRequest = 0x40,
+    };
+}
 
+namespace RXControlMask {
+    enum {
+        AcceptAny = 0x60,
+        AcceptOnlyExtendedID = 0x40,
+        AcceptOnlyStandardID = 0x20,
+        AcceptBUKT = 0x04,
+        AcceptAnyID = 0x00,
+    };
+}
 
-// Bits in the TXBnCTRL registers.
+namespace TXControlMask {
+    enum {
+        RequestInProcess = 0x08,
+        Error = 0x10
+    };
+}
 
-#define MCP_TXB_TXBUFE_M    0x80
-#define MCP_TXB_ABTF_M      0x40
-#define MCP_TXB_MLOA_M      0x20
-#define MCP_TXB_TXERR_M     0x10
-#define MCP_TXB_TXREQ_M     0x08
-#define MCP_TXB_TXIE_M      0x04
-#define MCP_TXB_TXP10_M     0x03
+namespace Status {
+    enum {
+        RX0InterruptFired = 0x01,
+        RX1InterruptFired = 0x02,
+        TX0InterruptFired = 0x08,
+        TX1InterruptFired = 0x20,
+        TX2InterruptFired = 0x80,
+        TX0Pending = 0x04,
+        TX1Pending = 0x10,
+        TX2Pending = 0x40,
+    };
+}
 
-#define MCP_TXB_RTR_M       0x40                                        // In TXBnDLC
-#define MCP_RXB_IDE_M       0x08                                        // In RXBnSIDL
-#define MCP_RXB_RTR_M       0x40                                        // In RXBnDLC
+namespace StatusMask {
+    enum {
+        TXPendingMask = 0x54,
+        TXInterruptMask = 0xA8,
+        RXInterruptMask = 0x03,
+    };
+}
 
-#define MCP_STAT_TX_PENDING_MASK (0x54)
-#define MCP_STAT_TX0_PENDING (0x04)
-#define MCP_STAT_TX1_PENDING (0x10)
-#define MCP_STAT_TX2_PENDING (0x40)
-#define MCP_STAT_TXIF_MASK   (0xA8)
-#define MCP_STAT_TX0IF       (0x08)
-#define MCP_STAT_TX1IF       (0x20)
-#define MCP_STAT_TX2IF       (0x80)
-#define MCP_STAT_RXIF_MASK   (0x03)
-#define MCP_STAT_RX0IF (1<<0)
-#define MCP_STAT_RX1IF (1<<1)
+namespace Error {
+    enum {
+        None = 0x0,
+        ControlError = 0x01,
+    };
+}
 
-#define MCP_EFLG_RX1OVR (1<<7)
-#define MCP_EFLG_RX0OVR (1<<6)
-#define MCP_EFLG_TXBO   (1<<5)
-#define MCP_EFLG_TXEP   (1<<4)
-#define MCP_EFLG_RXEP   (1<<3)
-#define MCP_EFLG_TXWAR  (1<<2)
-#define MCP_EFLG_RXWAR  (1<<1)
-#define MCP_EFLG_EWARN  (1<<0)
-#define MCP_EFLG_ERRORMASK  (0xF8)                                      // 5 MS-Bits
+namespace ErrorMask {
+    enum {
+        Any = 0b11111000
+    };
+}
 
-// Define MCP2515 register addresses
+namespace Register {
+    enum {
+        // RXF#@@@@ are filter registers, 0 through 5
+        // SID: standard identifier bits (16)
+        // EID: extended identifier bits (16)
+        RXF0SIDH = 0x00,
+        RXF0SIDL = 0x01,
+        RXF0EIDH = 0x02,
+        RXF0EIDL = 0x03,
+        RXF1SIDH = 0x04,
+        RXF1SIDL = 0x05,
+        RXF1EIDH = 0x06,
+        RXF1EIDL = 0x07,
+        RXF2SIDH = 0x08,
+        RXF2SIDL = 0x09,
+        RXF2EIDH = 0x0A,
+        RXF2EIDL = 0x0B,
+        RXF3SIDH = 0x10,
+        RXF3SIDL = 0x11,
+        RXF3EIDH = 0x12,
+        RXF3EIDL = 0x13,
+        RXF4SIDH = 0x14,
+        RXF4SIDL = 0x15,
+        RXF4EIDH = 0x16,
+        RXF4EIDL = 0x17,
+        RXF5SIDH = 0x18,
+        RXF5SIDL = 0x19,
+        RXF5EIDH = 0x1A,
+        RXF5EIDL = 0x1B,
+        Status = 0x0E,
+        Control = 0x0F,
+        // RXM#@@@@ are mask registers, 0 through 5
+        // SID: standard identifier bits (16)
+        // EID: extended identifier bits (16)
+        RXM0SIDH = 0x20,
+        RXM0SIDL = 0x21,
+        RXM0EIDH = 0x22,
+        RXM0EIDL = 0x23,
+        RXM1SIDH = 0x24,
+        RXM1SIDL = 0x25,
+        RXM1EIDH = 0x26,
+        RXM1EIDL = 0x27,
+        RateConfig3 = 0x28,
+        RateConfig2 = 0x29,
+        RateConfig1 = 0x2A,
+        InterruptEnable = 0x2B,
+        InterruptFlag = 0x2C,
+        ErrorFlag = 0x2D,
+        // Transmit/receive buffers
+        // 16 bytes each: [CTRL][<------ 15 bytes ------>]
+        // SIDH is the first content byte.
+        TXB0CTRL = 0x30,
+        TXB0SIDH = 0x31,
+        TXB1CTRL = 0x40,
+        TXB1SIDH = 0x41,
+        TXB2CTRL = 0x50,
+        TXB2SIDH = 0x51,
+        RXB0CTRL = 0x60,
+        RXB0SIDH = 0x61,
+        RXB1CTRL = 0x70,
+        RXB1SIDH = 0x71,
+    };
+}
 
-#define MCP_RXF0SIDH    0x00
-#define MCP_RXF0SIDL    0x01
-#define MCP_RXF0EID8    0x02
-#define MCP_RXF0EID0    0x03
-#define MCP_RXF1SIDH    0x04
-#define MCP_RXF1SIDL    0x05
-#define MCP_RXF1EID8    0x06
-#define MCP_RXF1EID0    0x07
-#define MCP_RXF2SIDH    0x08
-#define MCP_RXF2SIDL    0x09
-#define MCP_RXF2EID8    0x0A
-#define MCP_RXF2EID0    0x0B
-#define MCP_CANSTAT     0x0E
-#define MCP_CANCTRL     0x0F
-#define MCP_RXF3SIDH    0x10
-#define MCP_RXF3SIDL    0x11
-#define MCP_RXF3EID8    0x12
-#define MCP_RXF3EID0    0x13
-#define MCP_RXF4SIDH    0x14
-#define MCP_RXF4SIDL    0x15
-#define MCP_RXF4EID8    0x16
-#define MCP_RXF4EID0    0x17
-#define MCP_RXF5SIDH    0x18
-#define MCP_RXF5SIDL    0x19
-#define MCP_RXF5EID8    0x1A
-#define MCP_RXF5EID0    0x1B
-#define MCP_TEC         0x1C
-#define MCP_REC         0x1D
-#define MCP_RXM0SIDH    0x20
-#define MCP_RXM0SIDL    0x21
-#define MCP_RXM0EID8    0x22
-#define MCP_RXM0EID0    0x23
-#define MCP_RXM1SIDH    0x24
-#define MCP_RXM1SIDL    0x25
-#define MCP_RXM1EID8    0x26
-#define MCP_RXM1EID0    0x27
-#define MCP_CNF3        0x28
-#define MCP_CNF2        0x29
-#define MCP_CNF1        0x2A
-#define MCP_CANINTE     0x2B
-#define MCP_CANINTF     0x2C
-#define MCP_EFLG        0x2D
-#define MCP_TXB0CTRL    0x30
-#define MCP_TXB0SIDH    0x31
-#define MCP_TXB1CTRL    0x40
-#define MCP_TXB1SIDH    0x41
-#define MCP_TXB2CTRL    0x50
-#define MCP_TXB2SIDH    0x51
-#define MCP_RXB0CTRL    0x60
-#define MCP_RXB0SIDH    0x61
-#define MCP_RXB1CTRL    0x70
-#define MCP_RXB1SIDH    0x71
+namespace Buffer {
+    enum {
+        TX0 = Register::TXB0SIDH,
+        TX1 = Register::TXB1SIDH,
+        TX2 = Register::TXB2SIDH,
+        RX0 = Register::RXB0SIDH,
+        RX1 = Register::RXB1SIDH,
+    };
+}
 
-#define MCP_TX_INT      0x1C                                    // Enable all transmit interrup ts
-#define MCP_TX01_INT    0x0C                                    // Enable TXB0 and TXB1 interru pts
-#define MCP_RX_INT      0x03                                    // Enable receive interrupts
-#define MCP_NO_INT      0x00                                    // Disable all interrupts
+namespace InterruptMask {
+    enum {
+        TXAll = 0x1C,
+        TX01 = 0x0C,    // Only buffers 0 and 1
+        RXAll = 0x03,
+        None = 0x00
+    };
+}
 
-#define MCP_TX01_MASK   0x14
-#define MCP_TX_MASK     0x54
+namespace Instruction {
+    enum {
+        Fetch = 0x00,
+        Write = 0x02,
+        Read = 0x03,
+        Modify = 0x05,
+        LoadTX0 = 0x40,
+        LoadTX1 = 0x42,
+        LoadTX2 = 0x44,
+        ReadRX0 = 0x90,
+        ReadRX1 = 0x94,
+        ReadStatus = 0xA0,
+        Reset = 0xC0
+    };
+}
 
+namespace ControlMask {
+    enum {
+        Mode = 0xE0,
+    };
+}
 
-// Define SPI Instruction Set
+namespace Mode {
+    enum {
+        Normal = 0x00,
+        OneShot = 0x08,
+        AbortTX = 0x10,
+        Sleep = 0x20,
+        Loopback = 0x40,
+        ListenOnly = 0x60,
+        Config = 0x80,
+        Powerup = 0xE0,
+    };
+}
 
-#define MCP_WRITE       0x02
-#define MCP_READ        0x03
-#define MCP_BITMOD      0x05
-#define MCP_LOAD_TX0    0x40
-#define MCP_LOAD_TX1    0x42
-#define MCP_LOAD_TX2    0x44
-
-#define MCP_RTS_TX0     0x81
-#define MCP_RTS_TX1     0x82
-#define MCP_RTS_TX2     0x84
-#define MCP_RTS_ALL     0x87
-#define MCP_READ_RX0    0x90
-#define MCP_READ_RX1    0x94
-#define MCP_READ_STATUS 0xA0
-#define MCP_RX_STATUS   0xB0
-#define MCP_RESET       0xC0
-
-
-// CANCTRL Register Values
-
-#define MODE_NORMAL     0x00
-#define MODE_SLEEP      0x20
-#define MODE_LOOPBACK   0x40
-#define MODE_LISTENONLY 0x60
-#define MODE_CONFIG     0x80
-#define MODE_POWERUP    0xE0
-#define MODE_MASK       0xE0
-#define ABORT_TX        0x10
-#define MODE_ONESHOT    0x08
-#define CLKOUT_ENABLE   0x04
-#define CLKOUT_DISABLE  0x00
-#define CLKOUT_PS1      0x00
-#define CLKOUT_PS2      0x01
-#define CLKOUT_PS4      0x02
-#define CLKOUT_PS8      0x03
-
-
-// CNF1 Register Values
-
-#define SJW1            0x00
-#define SJW2            0x40
-#define SJW3            0x80
-#define SJW4            0xC0
-
-
-//  CNF2 Register Values
-
-#define BTLMODE         0x80
-#define SAMPLE_1X       0x00
-#define SAMPLE_3X       0x40
-
-
-// CNF3 Register Values
-
-#define SOF_ENABLE      0x80
-#define SOF_DISABLE     0x00
-#define WAKFIL_ENABLE   0x40
-#define WAKFIL_DISABLE  0x00
-
-
-// CANINTF Register Bits
-
-#define MCP_RX0IF       0x01
-#define MCP_RX1IF       0x02
-#define MCP_TX0IF       0x04
-#define MCP_TX1IF       0x08
-#define MCP_TX2IF       0x10
-#define MCP_ERRIF       0x20
-#define MCP_WAKIF       0x40
-#define MCP_MERRF       0x80
-
-// clock
-
-#define MCP_16MHz   1
-#define MCP_8MHz    2
-
-// speed 16M
+namespace InterruptFlag {
+    enum {
+        RX0 = 0x01,
+        RX1 = 0x02,
+        TX0 = 0x04,
+        TX1 = 0x08,
+        TX2 = 0x10,
+        Error = 0x20,
+        Wakeup = 0x40,
+    };
+}
 
 #define MCP_16MHz_1000kBPS_CFG1 (0x00)
 #define MCP_16MHz_1000kBPS_CFG2 (0xD0)
@@ -279,92 +281,6 @@
 #define MCP_16MHz_666kBPS_CFG2 (0xA0)
 #define MCP_16MHz_666kBPS_CFG3 (0x04)
 
-
-// speed 8M
-
-#define MCP_8MHz_1000kBPS_CFG1 (0x00)
-#define MCP_8MHz_1000kBPS_CFG2 (0x80)
-#define MCP_8MHz_1000kBPS_CFG3 (0x00)
-
-#define MCP_8MHz_500kBPS_CFG1 (0x00)
-#define MCP_8MHz_500kBPS_CFG2 (0x90)
-#define MCP_8MHz_500kBPS_CFG3 (0x02)
-
-#define MCP_8MHz_250kBPS_CFG1 (0x00)
-#define MCP_8MHz_250kBPS_CFG2 (0xb1)
-#define MCP_8MHz_250kBPS_CFG3 (0x05)
-
-#define MCP_8MHz_200kBPS_CFG1 (0x00)
-#define MCP_8MHz_200kBPS_CFG2 (0xb4)
-#define MCP_8MHz_200kBPS_CFG3 (0x06)
-
-#define MCP_8MHz_125kBPS_CFG1 (0x01)
-#define MCP_8MHz_125kBPS_CFG2 (0xb1)
-#define MCP_8MHz_125kBPS_CFG3 (0x05)
-
-#define MCP_8MHz_100kBPS_CFG1 (0x01)
-#define MCP_8MHz_100kBPS_CFG2 (0xb4)
-#define MCP_8MHz_100kBPS_CFG3 (0x06)
-
-#define MCP_8MHz_80kBPS_CFG1 (0x01)
-#define MCP_8MHz_80kBPS_CFG2 (0xbf)
-#define MCP_8MHz_80kBPS_CFG3 (0x07)
-
-#define MCP_8MHz_50kBPS_CFG1 (0x03)
-#define MCP_8MHz_50kBPS_CFG2 (0xb4)
-#define MCP_8MHz_50kBPS_CFG3 (0x06)
-
-#define MCP_8MHz_40kBPS_CFG1 (0x03)
-#define MCP_8MHz_40kBPS_CFG2 (0xbf)
-#define MCP_8MHz_40kBPS_CFG3 (0x07)
-
-#define MCP_8MHz_31k25BPS_CFG1 (0x07)
-#define MCP_8MHz_31k25BPS_CFG2 (0xa4)
-#define MCP_8MHz_31k25BPS_CFG3 (0x04)
-
-#define MCP_8MHz_20kBPS_CFG1 (0x07)
-#define MCP_8MHz_20kBPS_CFG2 (0xbf)
-#define MCP_8MHz_20kBPS_CFG3 (0x07)
-
-#define MCP_8MHz_10kBPS_CFG1 (0x0f)
-#define MCP_8MHz_10kBPS_CFG2 (0xbf)
-#define MCP_8MHz_10kBPS_CFG3 (0x07)
-
-#define MCP_8MHz_5kBPS_CFG1 (0x1f)
-#define MCP_8MHz_5kBPS_CFG2 (0xbf)
-#define MCP_8MHz_5kBPS_CFG3 (0x07)
-
-#define MCPDEBUG        (0)
-#define MCPDEBUG_TXBUF  (0)
-#define MCP_N_TXBUFFERS (3)
-
-#define MCP_RXBUF_0 (MCP_RXB0SIDH)
-#define MCP_RXBUF_1 (MCP_RXB1SIDH)
-
-#define MCP2515_SELECT()   digitalWrite(SPICS, LOW)
-#define MCP2515_UNSELECT() digitalWrite(SPICS, HIGH)
-
-#define MCP2515_OK         (0)
-#define MCP2515_FAIL       (1)
-#define MCP_ALLTXBUSY      (2)
-
-#define CANDEBUG   1
-
-#define CANUSELOOP 0
-
-#define CANSENDTIMEOUT (200)                                            // milliseconds
-
-
-// initial value of gCANAutoProcess
-
-#define CANAUTOPROCESS      (1)
-#define CANAUTOON           (1)
-#define CANAUTOOFF          (0)
-#define CAN_STDID           (0)
-#define CAN_EXTID           (1)
-#define CANDEFAULTIDENT     (0x55CC)
-#define CANDEFAULTIDENTEXT  (CAN_EXTID)
-
 #define CAN_5KBPS           1
 #define CAN_10KBPS          2
 #define CAN_20KBPS          3
@@ -384,16 +300,33 @@
 #define CAN_666KBPS         17
 #define CAN_1000KBPS        18
 
-#define CAN_OK              (0)
-#define CAN_FAILINIT        (1)
-#define CAN_FAILTX          (2)
-#define CAN_MSGAVAIL        (3)
-#define CAN_NOMSG           (4)
-#define CAN_CTRLERROR       (5)
-#define CAN_GETTXBFTIMEOUT  (6)
-#define CAN_SENDMSGTIMEOUT  (7)
-#define CAN_FAIL            (0xff)
+namespace MessageState {
+    enum {
+        MessagePending = 0x00,
+        MessageFetched = 0x01,
+        NoMessage = 0x02,
+    };
+}
 
-#define CAN_MAX_CHAR_IN_MESSAGE (8)
+namespace Result {
+    enum {
+        OK = 0x00,
+        Failed = 0x01,
+        AwaitBufferTimedOut = 0x02,
+        SendTimedOut = 0x03,
+        AllBuffersBusy = 0x04,
+    };
+}
+
+namespace Limit {
+    enum {
+        AwaitBufferTimeout = 0x32,
+        MessageBufferLength = 0x08,
+        TXBufferLength = 0x10,
+        TXBuffers = 0x03,
+    };
+}
+
+}
 
 #endif
