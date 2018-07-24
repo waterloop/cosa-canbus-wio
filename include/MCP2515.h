61f43e281af1f49d87417d367bc1c31b13810a74
Dmytro Shynkevych <dm.shynk@gmail.com>
2018-07-24 10:12:01 -0400

    Added rudimentary documentation to MCP2515.h (#3)

--- include/MCP2515.h
+++ include/MCP2515.h
@@ -9,13 +9,101 @@ namespace wlp {
     class MCP2515: public SPI::Driver {
     public:
         MCP2515(Board::DigitalPin cs_pin = Board::D10);
+        /*
+        ** Initialize the shield and set the transmission rate to `can_speed`.
+        **
+        ** - this must be called before any other method is called,
+        **   else the latter will fail silently
+        ** - `can_speed` must be one of the constants of the form CAN_[NUM]KBPS
+        **   defined in MCP2515_const.h
+        **
+        ** Return:
+        **   Result::OK on success
+        **   Result::Failed on failure
+        */
         uint8_t begin(uint8_t can_speed);
+        /*
+        ** Set filter number `num` to  `data`. Filters 0 through 5 are available.
+        **
+        ** This will result in only messages with `id` bitwise matching
+        ** `data` for at least one of the filters to be processed.
+        **
+        ** Return values match those of `begin`.
+        */
         uint8_t set_filter(uint8_t num, uint32_t data);
+        /*
+        ** Set mask number `num` to `data`. Masks 0 and 1 are available.
+        **
+        ** This will result in only the bits of `id` set to 1 in one of the masks
+        ** to be checked against the filters.
+        **
+        ** Return values match those of `begin`.
+        **
+        ** Examples follow; all masks and filters not given are assumed to be 0:
+        **
+        **   MASK_0 = 0x1FFFFFFF
+        **   FILTER_0 = 0x00001567
+        **     will match bits 0-28 of `id` against 0x00001567, i.e.
+        **     only messages with `id` == 0x00001567 will be processed
+        **
+        **   MASK_0 = 0x1FFFFFF0
+        **   MASK_1 = 0x1FFFFF00
+        **   FILTER_0 = 0x00001560
+        **     will match bits 4-28 of `id` against 0x00001560, i.e. messages
+        **     with `id` in the range 0x00001560...0x0000156F will be processed.
+        **     Note that MASK_1 is redundant here: MASK_0 | MASK_1 == MASK_0
+        **
+        **   FILTER_0 = 0x00001567
+        **   FILTER_1 = 0x00001569
+        **     this _would_ match only messages with `id` == 0x00001567
+        **     or `id` == 0x00001569, had MASK_0 or MASK_1 been set as needed.
+        **     With MASK_0 == 0x00000000 == MASK_1, this matches every message.
+        */
         uint8_t set_mask(uint8_t num, uint32_t data);
+        /*
+        ** Send buffer `buf` of length `len` in a message with id `id`.
+        **
+        ** - `len` shall be no greater than Limit::MessageBufferLength.
+        ** - `id` may be standard (only the lower 11 bits set)
+        **    or extended (29 lower bits are set).
+        **
+        **    Note: if a standard `id` is used, ensure that the extended
+        **    (upper 21) bits of all masks on the receiving side are set to zero.
+        **
+        ** Return:
+        **   Result::AwaitBufferTimedOut if waiting for a free buffer timed out
+        **   Result::SendTimedOut if sending the message timed out
+        **   Result::OK if the message was sent successfully
+        */
         uint8_t send_buffer(uint32_t id, uint8_t len, uint8_t *buf);
+        /*
+        ** Read a received message (there may be more than one at a time!)
+        ** and store its contents in `buf`, up to length `len`.
+        **
+        ** Return:
+        **   MessageState::MessageFetched on success
+        **   MessageState::NoMessage on failure
+        */
         void read_buffer(uint8_t len, uint8_t *buf);
+        /*
+        ** Get the current error status. Error::None indicates no error.
+        **
+        ** FIXME: Right now, every error is reported as Error::ControlError.
+        */
         uint8_t get_error();
+        /*
+        ** Check if a message is received an pending read.
+        **
+        ** Return:
+        **   MessageState::MessagePending if (at least one) message is pending
+        **   MessageState::NoMessage otherwise
+        */
         uint8_t get_message_state();
+        /*
+        ** Get the id of the last successfully read message.
+        **
+        ** The return value is unspecified if no message has been read yet.
+        */
         uint32_t get_id();
 
     private:
