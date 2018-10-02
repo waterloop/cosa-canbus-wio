6409c0ae2b641b648a52ed73042824f004c3de70
Mogball <jeffniu22@gmail.com>
2018-10-02 19:35:03 -0400

    Fixing new app

--- app-linux/src/main.cpp
+++ app-linux/src/main.cpp
@@ -1,7 +1,29 @@
 #include <sys/mcp2515.h>
 #include <MCP2515.h>
+#include <unistd.h>
 #include <stdio.h>
 
+using namespace wlp;
+
 int main(void) {
-    printf("Hello world\n");
+    linux::MCP2515 base("/dev/spidev0.0", 10000000);
+    base.begin();
+    MCP2515 bus(&base);
+    while (bus.begin(CAN_500KBPS, MCP_8MHz) != Result::OK) {
+        printf("CAN init failed, retrying\n");
+        sleep(1);
+    }
+    printf("CAN inited\n");
+
+    uint8_t buf[8];
+    while (true) {
+        if (bus.get_message_status() == MessageState::MessagePending) {
+            bus.read_buffer(8, buf);
+            printf("Data from %d\n", bus.get_id());
+            for (int i = 0; i < 8; ++i) {
+                printf("%02x ", buf[i]);
+            }
+            printf("\n");
+        }
+    }
 }
